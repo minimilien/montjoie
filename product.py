103751022d035bc03ebc23cdcb15681d8b844e3b
minimilien <47968684+minimilien@users.noreply.github.com>
2021-11-07 21:49:05 +0100

    bogue fix

--- product.py
+++ product.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from dataclasses import dataclass
 from tools import read_json
+from typing import List
 
 @dataclass
 class Product:
@@ -9,8 +10,14 @@ class Product:
     image:str
     comment:str
 
+    def get_comment(self) -> str:
+        if self.comment == None:
+            return ""
+        else:
+            return self.comment
 
-def load_products():
+
+def load_products() -> List[Product]:
     products = [Product(**product_dict) for product_dict in read_json("data/products.json")]
     if len(products)%2 == 1:
         products.append(Product(None,None,None,None))
